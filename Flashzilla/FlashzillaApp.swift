c7072c6771b5d92ff49940290c21f5547ec2bdb2
xHadi3 <hadiessam9@gmail.com>
2025-04-29 17:07:27 +0300

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Hadi Al zayer on 01/11/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
