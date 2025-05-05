6ed2694efa2ff66076e1621190a90f35a38d3765
xHadi3 <hadiessam9@gmail.com>
2025-05-05 13:43:53 +0300

    making the questions as stack cards which is movable

---
+++ Flashzilla/Card.swift
@@ -0,0 +1,8 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Hadi Al zayer on 04/11/1446 AH.
+//
+
+import Foundation
---
+++ Flashzilla/CardView.swift
@@ -0,0 +1,18 @@
+//
+//  CardView.swift
+//  Flashzilla
+//
+//  Created by Hadi Al zayer on 04/11/1446 AH.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardView()
+}
