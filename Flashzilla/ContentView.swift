ee0b19bf695216e7d528149f2167d939725ea724
xHadi3 <hadiessam9@gmail.com>
2025-05-05 13:44:15 +0300

    .

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -7,17 +7,46 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+extension View {
+    func stacked(at position: Int, in total: Int) -> some View {
+        let offset = Double(total - position)
+        return self.offset(y: offset * 10)
     }
 }
+
+
+struct ContentView: View {
+    
+    @State private var cards = Array<Card>(repeating: .example, count: 10)
+    
+    
+    var body: some View {
+        ZStack {
+            Image(.background)
+                .resizable()
+                .ignoresSafeArea()
+            VStack {
+                
+                ZStack{
+                    ForEach(0..<cards.count, id: \.self) { index in
+                        CardView(card: cards[index]) {
+                            withAnimation {
+                                removeCard(at: index)
+                            }
+                        }
+                        .stacked(at: index, in: cards.count)
+                    }
+                    
+                }
+        }
+    }
+}
+
+    func removeCard(at index: Int) {
+        cards.remove(at: index)
+    }
+}
+
 
 #Preview {
     ContentView()
